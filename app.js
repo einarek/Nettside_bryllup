a87c38cbb2e3e259d06716b6f6390d4c6fca2db3
einarek <einareke@hotmail.no>
2025-08-27 21:52:03 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -49,29 +49,3 @@ window.addEventListener("DOMContentLoaded", () => {
     document.head.appendChild(styleSheet);
   }
 });
-
-function initMap() {
-    const location = { lat: 58.43344530882086, lng: 8.747931217854964 }; 
-    const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
-    center: location,
-    });
-
-    const marker = new google.maps.Marker({
-    position: location,
-    map: map,
-    title: "Hisøy kirke",
-    });
-
-    const infoWindow = new google.maps.InfoWindow({
-    content: "<h3>Vielsen</h3><p>Hisøy kirke.</p>",
-    });
-
-    marker.addListener("click", () => {
-    infoWindow.open(map, marker);
-    });
-
-    infoWindow.open(map, marker);
-}
-
-
